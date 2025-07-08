99124383a62dd9b00e103759412a5ae3f88e08ca
keroppi <yejiseo0725@gmail.com>
2025-07-08 17:10:34 +0900

    detail JS

--- js/detail.js
+++ js/detail.js
@@ -1,5 +1,18 @@
 gsap.registerPlugin(ScrollTrigger);
 
+$(function () {
+  Splitting();
+
+  gsap.from(".main h1 .char", {
+    y: 100, // 아래에서 올라오게
+    opacity: 0, // 안보이다가
+    stagger: 0.04, // 한 글자씩 빠르게
+    ease: "power4.out", // 부드러운 느낌
+    duration: 0.8,
+    delay: 0.2, // 약간의 딜레이 후 실행
+  });
+});
+
 // slick-slider
 $(function () {
   $(".slide-area").slick({
