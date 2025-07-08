2cc7636762b15e13b2dd8830aa383ce74df213bd
keroppi <yejiseo0725@gmail.com>
2025-07-08 15:16:49 +0900

    details for company p.j

--- js/detail.js
+++ js/detail.js
@@ -1,18 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// white-section 을 만나면 색이 변하는 header
-document.querySelectorAll(".white-section").forEach((section) => {
-  ScrollTrigger.create({
-    trigger: section,
-    start: "top 10%",
-    end: "bottom 10%",
-    onEnter: () => document.body.classList.add("light-header"),
-    onEnterBack: () => document.body.classList.add("light-header"),
-    onLeave: () => document.body.classList.remove("light-header"),
-    onLeaveBack: () => document.body.classList.remove("light-header"),
-  });
-});
-
 // slick-slider
 $(function () {
   $(".slide-area").slick({
